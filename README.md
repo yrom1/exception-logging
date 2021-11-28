cf814c1a70b931631f3029e4e5df47bc009d7924
yrom1 <ryanm.inbox@gmail.com>
2021-11-28 14:10:29 -0500

    Add `make_README.py` and helpful `README.md`.

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # exception-logging-decorator
-Exception logging decorator.
+The following function:
+```
+@log
+def no_return() -> NoReturn:
+    raise Exception("DO NOT PASS GO DO NOT COLLECT $200.")
+```
+Logs the following exception in `log.log`.
+```
+ERROR:root:Exception raised in function "no_return"
+ called with args "" at "2021-11-28 13:49:49.891050-05:00".
+ exception: "DO NOT PASS GO DO NOT COLLECT $200."
+Traceback (most recent call last):
+  File "main.py", line 48, in wrapper
+    output = function(*args, **kwargs)
+  File "main.py", line 64, in no_return
+    raise Exception("DO NOT PASS GO DO NOT COLLECT $200.")
+Exception: DO NOT PASS GO DO NOT COLLECT $200.
+
+```
---
+++ make_README.py
@@ -0,0 +1,22 @@
+from typing import TextIO
+
+
+def write_readme_block(file: TextIO, content: str, markup_language: str = "") -> None:
+    file.write(f"\n```{markup_language}\n")
+    file.write(content)
+    file.write(f"\n```\n")
+
+
+with open("log.log", "r") as log:
+    log_contents = log.read()
+
+TEST_FUNCTION = """@log
+def no_return() -> NoReturn:
+    raise Exception("DO NOT PASS GO DO NOT COLLECT $200.")"""
+
+with open("README.md", "w") as README:
+    README.write("# exception-logging-decorator\n")
+    README.write("The following function:")
+    write_readme_block(README, TEST_FUNCTION)
+    README.write("Logs the following exception in `log.log`.")
+    write_readme_block(README, log_contents)
