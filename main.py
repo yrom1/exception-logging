1b0d02701da8fdf2fab9ada5b54a4137d6f21c64
yrom1 <ryanm.inbox@gmail.com>
2021-11-28 13:39:02 -0500

    Added test of logger and log file.

--- main.py
+++ main.py
@@ -3,7 +3,9 @@
 import datetime
 import functools
 import logging
+import pathlib
 import sys
+from typing import Any, Callable, NoReturn, TypeVar
 
 if (sys.version_info.major, sys.version_info.minor) < (3, 6):
     raise Exception("Must be using Python 3.6 or higher for zoneinfo.")
@@ -12,11 +14,10 @@ elif sys.version_info.minor < 9:
 else:
     import zoneinfo  # type: ignore
 
-logging.basicConfig(filename="log.log", filemode="a", level=logging.ERROR)
-LOGGER = logging.getLogger()
+F = TypeVar("F", bound=Callable[..., Any])
 
 
-def log(function):
+def log(function: F) -> F:
     """Exception logging decorator.
     Uses "US/Eastern" time zone by default, for other zones see:
         https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
@@ -37,8 +38,31 @@ def log(function):
             return output
         except Exception as e:
             LOGGER.exception(
-                f'Exception raised in function "{function.__qualname__}" called with args ->{signature}<- at "{timestamp}". exception: {str(e)}'
+                (
+                    f'Exception raised in function "{function.__qualname__}"'
+                    f' called with args "{signature}" at "{timestamp}".'
+                    f' exception: "{str(e)}"'
+                )
             )
             raise e
 
-    return wrapper
+    return wrapper  # type: ignore
+
+
+@log
+def no_return() -> NoReturn:
+    raise Exception("DO NOT PASS GO DO NOT COLLECT $200.")
+
+
+LOGGING_FILENAME = "log.log"
+LOG_FILE = pathlib.Path(".") / LOGGING_FILENAME
+try:
+    LOG_FILE.unlink()
+except FileNotFoundError:
+    pass
+logging.basicConfig(filename=LOGGING_FILENAME, filemode="a", level=logging.ERROR)
+LOGGER = logging.getLogger()
+try:
+    no_return()
+except:
+    pass
